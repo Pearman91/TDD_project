f19d6180f6a7c1c3bc4c3358cfd04e7127ebad8e
Jan Hruška <hru3kus@gmail.com>
2020-03-27 19:09:50 +0100

    test item input

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -12,8 +15,27 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retreive_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        self.fail(
-            'Finish the test - there are still things that have to be tested!')
+
+        # Is there the right h1 tag text?
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Is there a right element for adding items?
+        inputbox = self.browser.find_element_by_id('id_new_items')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a To-Do item')
+
+        # user adds item
+        inputbox.send_keys('Buy new shoes')  # selenium input
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # Is there table with newly added item?
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy new shoes') for row in rows)
+
+        # TODO: chapter 04
 
 
 if __name__ == '__main__':
