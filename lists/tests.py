e752701223baa20942e9f3c638bc77049c3098e5
Jan Hruška <hru3kus@gmail.com>
2020-03-27 16:12:17 +0100

    add and test home page http response

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
@@ -10,3 +11,10 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)  # instance of HttpResponse
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def home_page():
-    return None
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
