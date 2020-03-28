a544f3c9135350e920e8fa248fe96427f5c1a906
Jan Hruška <hru3kus@gmail.com>
2020-03-28 07:56:33 +0100

    refactor home page to use template

--- lists/tests.py
+++ lists/tests.py
@@ -8,13 +8,33 @@ from lists.views import home_page
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
+        """
+        This test is redundant after adding
+        test_home_page_returns_correct_html_with_test_client, that automatically
+        tests url resolve.
+        """
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
+        """
+        This is non-Djangoish version of the test, since it doesn't use
+        TestCase.client
+        """
         request = HttpRequest()
         response = home_page(request)  # instance of HttpResponse
         html = response.content.decode('utf8')
+
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
+
+    def test_home_page_returns_correct_html_with_test_client(self):
+        """ Use Django Test Client. Testing of html content = testing constants,
+         which is wrong. Test whether the correct template was used, instead.
+         We don't need to decode html content here.
+         """
+        response = self.client.get('/')
+
+        # behold the magic power of Django's test client!
+        self.assertTemplateUsed(response, 'index.html')
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'index.html')
