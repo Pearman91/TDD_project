b065b2ac0e31fa0fece683affd08278aa4b3ce51
Jan Hruška <hru3kus@gmail.com>
2020-03-28 08:21:21 +0100

    fill in index html; correct typos

--- functional_tests.py
+++ functional_tests.py
@@ -12,7 +12,7 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def test_can_start_a_list_and_retreive_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
 
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
         # Is there a right element for adding items?
         inputbox = self.browser.find_element_by_id('id_new_items')
         self.assertEqual(
-            inputbox.get_attribute('placeholder'), 'Enter a To-Do item')
+            inputbox.get_attribute('placeholder'), 'Just write down sth.')
 
         # user adds item
         inputbox.send_keys('Buy new shoes')  # selenium input
@@ -33,9 +33,8 @@ class NewVisitorTest(unittest.TestCase):
         # Is there table with newly added item?
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy new shoes') for row in rows)
-
-        # TODO: chapter 04
+        self.assertTrue(any(row.text == '1: Buy new shoes' for row in rows),
+                        "New item was not added to the list.")
 
 
 if __name__ == '__main__':
