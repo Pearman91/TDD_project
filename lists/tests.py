e67c4c7d7f03e6c340c1a6c190e60260059437ac
Jan Hruška <hru3kus@gmail.com>
2020-03-29 15:48:56 +0200

    create Item model + migrations

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
+from lists.models import Item
 from lists.views import home_page
 
 
@@ -43,3 +44,24 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'New list item'})
         self.assertIn('New list item', response.content.decode())
         self.assertTemplateUsed(response, 'index.html')
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first bloody item in da list'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second item in the list'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text,
+                         'The first bloody item in da list')
+        self.assertEqual(second_saved_item.text, 'The second item in the list')
