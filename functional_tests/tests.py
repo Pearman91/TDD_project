a2f7482dc48049f125f8665b0a62449f88e7c331
Jan Hruška <hru3kus@gmail.com>
2020-03-31 19:14:28 +0200

    FT for style

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -85,6 +85,25 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Climb Mt. Everest', page_text)
         self.assertNotIn('Buy new shoes', page_text)
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # check that inputbox is centered
+        inputbox = self.browser.find_element_by_id('id_new_items')
+        # TODO: check why this doesn't work
+        # self.assertAlmostEqual(
+        #     inputbox.location['x'] + inputbox.size['width']/2,
+        #     512,
+        #     delta=10)
+
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_items')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2, 512, delta=10)
+
     def wait_for_row_in_table(self, row_text):
         start_time = time.time()
         while True:
