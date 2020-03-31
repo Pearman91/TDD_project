7721a400ed6872fd845caa5284068463e4553ae7
Jan Hruška <hru3kus@gmail.com>
2020-03-31 19:37:11 +0200

    use base template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -88,14 +88,13 @@ class NewVisitorTest(LiveServerTestCase):
     def test_layout_and_styling(self):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
-
+        time.sleep(0.5)
         # check that inputbox is centered
         inputbox = self.browser.find_element_by_id('id_new_items')
-        # TODO: check why this doesn't work
-        # self.assertAlmostEqual(
-        #     inputbox.location['x'] + inputbox.size['width']/2,
-        #     512,
-        #     delta=10)
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10)
 
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
