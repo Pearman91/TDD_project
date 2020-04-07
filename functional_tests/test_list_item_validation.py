c7fa1f1f26ed0811dfe299ce18946e0a1faa3d1c
Jan Hruška <hru3kus@gmail.com>
2020-04-07 09:54:03 +0200

    change FT so it reflects html5 input validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -10,33 +10,28 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.live_server_url)
 
-        # submit empty item and get appropriate error message
+        # submit empty item and get html5 error message
         self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            'Nana, we won\'t let you put in empty items.'
-        ))
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
+            '#id_text:invalid'))
 
         # submit non-empty item
-        self.get_item_input_box().send_keys(
-            "Dance like nobody's watching")
+        self.get_item_input_box().send_keys("Dance like nobody's watching")
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
+            '#id_text:valid'))
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_table("1: Dance like nobody's watching")
 
         # for the 2nd time submit empty item and get error message
         self.get_item_input_box().send_keys(Keys.ENTER)
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            'Nana, we won\'t let you put in empty items.'
-        ))
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
+            '#id_text:invalid'))
 
         # add one more correct item and check both items
-        self.get_item_input_box().send_keys(
-            "And sing like nobody's listening")
+        self.get_item_input_box().send_keys("And sing like nobody's listening")
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
+            '#id_text:valid'))
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_table("1: Dance like nobody's watching")
         self.wait_for_row_in_table("2: And sing like nobody's listening")
-
-
-        self.fail('Write this test, dammit!')
 
